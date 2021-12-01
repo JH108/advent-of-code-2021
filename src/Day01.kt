d013caf9cec1c103bb387413c940133e2ca9f676
Jesse Hill <jesse_hill@eogresources.com>
2021-12-01 12:53:57 -0600

    Solution for Day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,7 +13,19 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windows = input
+            .toInts()
+            .windowed(3, 1)
+        var previousWindowSize = windows.first().sum()
+
+        return windows.fold(0) { total, window ->
+            val windowSum = window.sum()
+            val increase = if (windowSum > previousWindowSize) total + 1 else total
+
+            previousWindowSize = windowSum
+
+            increase
+        }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -23,5 +35,9 @@ fun main() {
     val input = readInput("Day01")
     val part1Result = part1(input)
     println("Part1: $part1Result")
-    println(part2(input))
+
+    check(part2(testInput) == 5)
+
+    val part2Result = part2(input)
+    println("Part2: $part2Result")
 }
