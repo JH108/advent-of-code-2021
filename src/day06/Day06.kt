ff5a1d32ca71754fb7818a000237ca365fd1dd27
Jesse Hill <jessehill108@gmail.com>
2021-12-09 14:53:43 -0600

    Day 6 part 2 solution

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -7,23 +7,19 @@ const val inputFilename = "day06/Day06"
 
 fun main() {
     fun part1(input: List<String>, days: Int = 80): Int {
-        var lanternFish = input.flatMap { it.split(",").map { LanternFish(it.toInt()) } }
+        val lanternFish = input.flatMap { it.split(",").map { LanternFish(it.toInt()) } }
 
-        for (day in 1..days) {
-            lanternFish = lanternFish.map { fish ->
-                fish.growOlder()
-            }
-        }
-
-        val allDescendants = lanternFish.sumOf {
-            it.countDescendants()
-        }
-
-        return allDescendants + lanternFish.size
+        return lanternFish.size + lanternFish.map { it.growByDays(days) }.sumOf { it.countDescendants() }
     }
 
-    fun part2(input: List<String>): Int {
-        return 1
+    fun part2(input: List<String>, days: Int = 80): Long {
+        val lanternFish = input.first().split(",")
+        val lanternFishInTheOcean = LanternFishInTheOcean.parse(
+            input = lanternFish,
+            timeSinceTheBeginning = days
+        )
+
+        return lanternFishInTheOcean.multiply()
     }
 
     val testInput = readInput(testInputFilename)
@@ -46,13 +42,13 @@ fun main() {
 
     check(part1Result == part1Answer)
 
-    val part2TestAnswer = null
-    val part2Answer = null
-    val part2TestResult = part2(testInput)
+    val part2TestAnswer = 26984457539
+    val part2Answer = 1687617803407
+    val part2TestResult = part2(testInput, 256)
 
     check(part2TestResult == part2TestAnswer)
 
-    val part2Result = part2(input)
+    val part2Result = part2(input, 256)
     println("Part2: $part2Result")
 
     check(part2Result == part2Answer)
--- src/day06/Day06Logic.kt
+++ src/day06/Day06Logic.kt
@@ -1,9 +1,16 @@
 package day06
 
+import utils.toInts
+
 data class LanternFish(
     val timeUntilNextSpawn: Int,
     val descendants: List<LanternFish> = listOf()
 )
+
+fun LanternFish.growByDays(days: Int) = (1..days)
+    .fold(this) { lanternFish, _ ->
+        lanternFish.growOlder()
+    }
 
 fun LanternFish.growOlder(): LanternFish {
     val timeToSpawn = timeUntilNextSpawn - 1 < 0
@@ -20,3 +27,25 @@ fun LanternFish.growOlder(): LanternFish {
 
 fun LanternFish.countDescendants(): Int =
     descendants.size + descendants.sumOf { it.countDescendants() }
+
+data class LanternFishInTheOcean(val timeSinceTheBeginning: Int, var fish: LongArray) {
+    companion object {
+        fun parse(input: List<String>, timeSinceTheBeginning: Int): LanternFishInTheOcean {
+            val initialFish = input.toInts().fold(LongArray(9)) { fishIndex, fish ->
+                fishIndex.apply {
+                    this[fish] += 1L
+                }
+            }
+
+           return LanternFishInTheOcean(timeSinceTheBeginning, initialFish)
+        }
+    }
+}
+
+fun LanternFishInTheOcean.multiply(): Long {
+    return (1..timeSinceTheBeginning).fold(fish) { totalFish, _ ->
+        val fishHavingOffspring = totalFish.first()
+
+        totalFish.drop(1).toLongArray().apply { this[6] += fishHavingOffspring } + longArrayOf(fishHavingOffspring)
+    }.sum()
+}
